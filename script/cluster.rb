4cd6d71581f7a8205d26461b8b34c54f5ef45404
Todd Fisher <taf2@glazgo.(none)>
2009-08-07 11:41:35 -0400

    refactored clustering

--- script/cluster.rb
+++ script/cluster.rb
@@ -10,17 +10,28 @@ require File.join(RAILS_ROOT,'lib','normalize_tags')
 class Text
   PRUNE=(Post.excluded_tags + ['INSIDE STORY:','PHOTO GALLERY:','VIDEO:','POLL:','SYTYCD:','UPDATE:']).freeze
 
-  def self.post_text(post,tagger=nil)
-    title = post.title
+  #
+  # extract terms from post, include the month-day timestamp as a special term - since news articles are generally time based
+  #
+  def self.post_words(post,tagger=nil)
+    # make sure we're in the same char encoding
+    title = Normalize::Tags.prepare_text(post.title)
+    body = Normalize::Tags.prepare_text(post.plain_text)
+
+    # remove labels
     PRUNE.each {|p| title.gsub!(p,'') } # remove common no-op words
-    text = Normalize::Tags.prune_stopwords(title) + ' ' + post.tag_list.join(' ') + ' ' + Normalize::Tags.prune_stopwords(post.plain_text) 
-    text = Normalize::Tags.prepare_text(text)
-    if tagger
-      tagger.nouns(text).map{|t| t.first.stem.downcase }.join(' ')
-    else
-      text.downcase
-    end
-#    post.tag_list.join(' ')
+    PRUNE.each {|p| body.gsub!(p,'') } # remove common no-op words
+
+    # break by sentence and space
+    title = title.strip.split('.').map{|s| s.split(' ') }.flatten.join(' ')
+    body = body.strip.split('.').map{|s| s.split(' ') }.flatten.join(' ')
+
+    # extract only nouns
+    title_words = tagger.nouns(title).map{|t| t.first.stem.downcase }
+    body_words  = tagger.nouns(body ).map{|t| t.first.stem.downcase }
+
+    # add the date timestamp as a tag to relate docs by time e.g. any article published on tuesday is more likely to be related then friday and tuesday...
+    [post.created_at.strftime("%F")] + title_words + post.tag_list + body_words
   end
 end
 
@@ -29,7 +40,7 @@ class Model < Lda::Lda
     super()
     @corpus = corpus
     self.num_topics = (corpus.posts.size/2)
-    self.max_iter = 200
+    self.max_iter = 50
     self.corpus = corpus
     self.load_vocabulary(corpus.vocab_vec)
   end
@@ -38,7 +49,35 @@ class Model < Lda::Lda
     discover_topics
     doc_prob = self.compute_topic_document_probability
     topics = self.top_words
-    puts topics.inspect
+    # document array with array of topics probability
+    cluster = {}
+    doc_prob.each_with_index do|topic_probs, doc_idx|
+      doc = @corpus.posts[doc_idx]
+      tp_ordered = []
+      topic_probs.each_with_index do|tp,idx|
+        tp_ordered << {:prob => tp.abs, :id => idx}
+      end
+      tp_ordered = tp_ordered.sort_by {|v| v[:prob] }
+      most_likely_topic = tp_ordered.last
+      # detailed inspect
+      #puts "#{topics[most_likely_topic[:id]].inspect} - #{most_likely_topic[:prob]} - #{doc.title} - #{tp_ordered.inspect}"
+      id = most_likely_topic[:id]
+      cluster[id] ||= {}
+      cluster[id][:docs] ||= []
+      cluster[id][:words] ||= topics[id]
+      cluster[id][:docs] << doc
+      #puts "#{most_likely_topic[:id]} - #{doc.title}"
+    end
+#    for c in cluster do
+#      yield c[:words], c[:docs]
+#    end
+    cluster.each do|cid,info|
+      puts "cluster: #{cid}- #{info[:words].inspect}"
+      info[:docs].each do|doc|
+        puts "\t#{doc.title}"
+      end
+    end
+    #puts topics.inspect
   end
 
 protected
@@ -53,17 +92,21 @@ class Corpus < Lda::Corpus
   def initialize
     super
 
+    puts "loading tagger lexicon..."
     @rule_tagger = Brill::Tagger.new
 
+    puts "loading new posts..."
     # find the last 100 posts
     @posts = Post.find(:all, :limit => Post.count, :order => 'created_at DESC')
 
+    puts "building vocabulary..."
     # build the vocabulary from the tags
     build_vocab
 
     # create the tagger
     @tagger = Word::Tagger.new( @vocab_vec, :words => 4 )
 
+    puts "loading documents..."
     @docs = []
     for post in @posts do
       doc = PostDoc.new(post, @vocab_set, @tagger, @rule_tagger)
@@ -80,12 +123,14 @@ class Corpus < Lda::Corpus
   end
 
   def doc_freq_by_term(term)
+    matching = @docs.select{|doc| doc.include?(term) }
+    matching.count
   end
 
 protected
   def build_vocab
     vec = []  #Tag.all.map{|t| t.name }
-    @posts.each {|p| vec << Text::post_text(p, @rule_tagger).split(' ') }
+    @posts.each {|p| vec += Text::post_words(p, @rule_tagger) }
     vec.flatten!
     vec.uniq!
     puts "Vocab size: #{vec.size}"
@@ -98,10 +143,19 @@ protected
 end
 
 class PostDoc < Lda::Document
+  attr_reader :term_freq
   def initialize(post, vocab_set, tagger, rtagger)
-    text = Text::post_text(post,rtagger)
-    @term_freq = tagger.freq(text)
-    puts @term_freq.inspect
+    words = Text::post_words(post,rtagger)
+    @term_freq = {}#tagger.freq(text)
+    for w in words do
+      if @term_freq.key?(w)
+        @term_freq[w] += 1
+      else
+        @term_freq[w] = 1
+      end
+    end
+
+    #puts @term_freq.inspect
     idx = "#{@term_freq.size} "
     @post = post
     # see: lda-ruby/lib/lda.rb: 66
@@ -111,32 +165,42 @@ class PostDoc < Lda::Document
     #   5 1:2 3:1 4:2 7:3 12:1
     #
     @term_freq.each {|w,f| idx += "#{vocab_set[w]}:#{f} " }
+    @total_frequency = @term_freq.values.inject(0){|a,b|a+b} 
     super(idx)
+  end
+
+  def include?(word)
+    @term_freq.key?(word)
   end
 
   # see: http://oldmoe.blogspot.com/2008/08/document-matching-in-ruby.html
   def tf_idf(corpus)
-    total_frequency = @term_freq.values.inject(0){|a,b|a+b}
+    magnitude = 0
     @term_freq.each do |term,freq|
-      @term_freq[term] = (freq / total_frequency) * self.df(term) #assume we have a method that returns the document frequency value for any term
-      magnitude = magnitude + (@term_freq[term]**2)
+      @term_freq[term] = (freq / @total_frequency) * corpus.doc_freq_by_term(term) #assume we have a method that returns the document frequency value for any term
+      magnitude += (@term_freq[term]**2)
     end
     [@term_freq, magnitude]
   end
 
-  def match(doc)
+  # see: http://oldmoe.blogspot.com/2008/08/document-matching-in-ruby.html
+  def cosine_similarity(doc)
     my_tf_idf,  my_magnitude  = self.tf_idf
     his_tf_idf, his_magnitude = doc.tf_idf
+
     dot_product = 0
     my_tf_idf.each do |term,tf_idf|
-      dot_product = dot_product + tf_idf * his_tf_idf[term] if his_tf_idf[term]
+      dot_product += (tf_idf * his_tf_idf[term]) if his_tf_idf[term]
     end
-    cosine_similarity = dot_product / (my_magnitude * his_magnitude)
+
+    dot_product / Math.sqrt(my_magnitude * his_magnitude)
   end
 
 end
 
-Topic.destroy_all # for testing - only
+#XXX: removing topics for testing ONLY XXX
+Topic.destroy_all # for testing - ONLY XXX
+#XXX: removing topics for testing ONLY XXX
 
 # load the corpus
 corpus = Corpus.new
